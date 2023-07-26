459ad17e12b389ac67a728c3cf14947745e092fb
kirubagracy <kirubachemklt@gmail.com>
2023-07-26 12:39:13 +0530

    new project structure

---
+++ src/test/java/org/sample/TestGit.java
@@ -0,0 +1,13 @@
+package org.sample;
+
+public class TestGit {
+	public static void main(String[] args) {
+		System.out.println("kiruba");
+		System.out.println("kowsi");
+		System.out.println("besties");
+		
+		
+	}
+	
+
+}
